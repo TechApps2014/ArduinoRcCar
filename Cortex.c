8baba70762791a0555861b304f844b3334ea84f8
TechApps2014 <joho713@yahoo.com>
2014-03-10 13:22:00 -0500

    Create Cortex.c

---
+++ Cortex.c
@@ -0,0 +1,16 @@
+task main()
+{
+	boolean Safty = true;
+	if (Btn6U == true){
+	}
+
+	if (Safty == false){	// if safty is on then run 
+		motor[port4] = vexRT(3); 
+		motor[port5] = vexRT(2);
+		
+		if (Btn6U == true){ // turn safty back on
+			Safty = true;
+		}
+	}
+
+}
