a8ec71a51e07f5557e7d26c83fa259f093f851c2
TechApps2014 <joho713@yahoo.com>
2014-03-13 13:43:19 -0500

    Update and rename Cortexts_V1.1.c to Cortext_V1.1.c

--- Cortext_V1.1.c
+++ Cortext_V1.1.c
@@ -1,9 +1,8 @@
-
 // We tried using other code V1.0 and it did not work so we used this code fo
 // basic teleOp and movement
 task main()
 {
   while(true){
-	  motor[port4] = vexRT(3); 
-		motor[port5] = vexRT(2);
+	motor[port4] = vexRT(3); 
+	motor[port5] = vexRT(2);
 }
