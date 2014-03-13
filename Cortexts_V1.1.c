af1fa99808843ced97abdea14ab069f05225c017
TechApps2014 <joho713@yahoo.com>
2014-03-13 13:42:08 -0500

    Create Cortexts_V1.1.c

---
+++ Cortexts_V1.1.c
@@ -0,0 +1,9 @@
+
+// We tried using other code V1.0 and it did not work so we used this code fo
+// basic teleOp and movement
+task main()
+{
+  while(true){
+	  motor[port4] = vexRT(3); 
+		motor[port5] = vexRT(2);
+}
